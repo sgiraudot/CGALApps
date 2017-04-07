6bddc23a0811a4d262e351c12a28973377b0cc02
Simon Giraudot <simon.giraudot@geometryfactory.com>
2017-04-07 09:34:51 +0200

    Add function to write surface

--- include/io.h
+++ include/io.h
@@ -52,6 +52,36 @@ void write_point_set (const Args& args, Point_set& points)
   }
 }
 
+template <typename FacetRange>
+void write_surface_to_stream (Point_set& points,
+                              const FacetRange& facets,
+                              std::ostream& stream)
+{
+  stream << "OFF\n" << points.size() << " " << facets.size() << " 0\n";
+  for (typename Point_set::const_iterator it = points.begin(); it != points.end(); ++ it)
+    stream << points.point(*it) << std::endl;
+  for (typename FacetRange::const_iterator it = facets.begin(); it != facets.end(); ++ it)
+    stream << "3 " << (*it)[0] << " " << (*it)[1] << " " << (*it)[2] << std::endl;
+
+}
+
+template <typename FacetRange>
+void write_surface (const Args& args, Point_set& points,
+                    const FacetRange& facets)
+{
+  // Output file can be given with -o or --output
+  std::string filename = args.get_string ('o', "output");
+
+  // Otherwise, write to std::cout
+  if (filename == std::string())
+    write_surface_to_stream (points, facets, std::cout);
+  else
+  {
+    std::ofstream f(filename.c_str());
+    write_surface_to_stream (points, facets, f);
+  }
+}
+
 
 }
 
